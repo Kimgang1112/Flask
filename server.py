9cc4bccc256933d4e50c1479c842c7cd58459310
kimgang <yangsangchoo1112@gmail.com>
2025-09-04 22:53:03 +0900

    함수 사용 완료

--- server.py
+++ server.py
@@ -2,12 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def a(x):
+    return int(x)+1
+
+
+
 @app.route('/')
 def index():
     return 'hello'
 
 @app.route('/read/<id>/')
 def read(id):
-    return f'read {id}'
+
+    return str(a(id))
 
 app.run(port=5001,debug=True)
