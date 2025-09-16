f1d2cd4afa65adaac20cfdc45a5856d4a9ece9c9
kimgang <yangsangchoo1112@gmail.com>
2025-09-16 23:42:07 +0900

    pymysql사용

--- server.py
+++ server.py
@@ -1,23 +1,53 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, request, render_template
+import pymysql
+
 
 app = Flask(__name__)
 
-def a(x):
-    return int(x)+1
+# DB 연결 설정
+db_config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "q1w2e3",
+    "database": "Study",
+    "charset": "utf8mb4"
+}
 
-
-
-@app.route('/')
+# 홈 페이지
+@app.route("/")
 def index():
-    return '''
-    <h1>안녕하세요</h1>
-    저는 김강입니다.
+    return render_template("index.html")
 
-    '''
+# 데이터 저장
+@app.route("/submit", methods=["POST"])
+def save_db():
+    value = request.form.get("value")   # form 데이터 받기
 
-@app.route('/read/<id>/')
-def read(id):
+    # DB 연결
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
 
-    return redirect(url_for('index'))
+    # 값 추가 (numcount 테이블의 num 컬럼)
+    sql = "INSERT INTO numcount (num) VALUES (%s)"
+    cursor.execute(sql, (value,))
 
-app.run(port=5001,debug=True)
+    conn.commit()
+    conn.close()
+
+    return f"DB에 값 {value} 저장 완료!"
+
+# 데이터 조회
+@app.route("/list")
+def list_data():
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM numcount")
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return {"data": rows}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
